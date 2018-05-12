bb67c4944eb5750a2fe353fb54807632e57f84be
Angus Yuen <angus.yuen29@gmail.com>
2018-05-12 12:08:06 +1000

    Style changes

--- plot.py
+++ plot.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_bar_graph(all_users, number_of_users, title, xlabel):
-	x = []
-	y = []
+def create_axis_arrays(all_users, number_of_users):
+	x = []	# usernames
+	y = []	# values
 
 	s = [(k, all_users[k]) for k in sorted(all_users, key=all_users.get, reverse=True)]
-	
 	i = 0
 	for k, v in s:
 		if i >= number_of_users:
@@ -15,18 +14,23 @@ def plot_bar_graph(all_users, number_of_users, title, xlabel):
 		y.append(v)
 		i += 1
 
-	print(x)
-	print(y)
-	fig, ax = plt.subplots(figsize=(13, 6))
+	return x, y
+
+
+def plot_bar_graph(all_users, number_of_users, title, xlabel):
+	x, y = create_axis_arrays(all_users, number_of_users)
+
+	_, ax = plt.subplots(figsize=(13, 6))
 	for i, v in enumerate(y):
 		ax.text(v + 1.5, i, str(v), color='blue')
 
-	ax.barh(np.arange(len(x)), y, align='center', color="blue")
-	ax.set_yticks(np.arange(len(y)))
+	ax.barh(np.arange(len(x)), width=y, align='center', color="blue")
+	ax.set_yticks(np.arange(len(x)))
 	ax.set_yticklabels(x, minor=False)
+	ax.invert_yaxis()
+
 	plt.title(title)
 	plt.xlabel(xlabel)
-	plt.gca().invert_yaxis()
 	plt.show()
 
 
